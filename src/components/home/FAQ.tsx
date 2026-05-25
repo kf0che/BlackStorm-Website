0136989d4a87914abbb896a13395ee1c3a1881f0
kf0che <kf0che@protonmail.com>
2026-05-24 22:44:44 -0500

    fix: resolve layout overlap issues

--- src/components/home/FAQ.tsx
+++ src/components/home/FAQ.tsx
@@ -67,8 +67,7 @@ export default function FAQ() {
                     strokeLinejoin="round"
                     aria-hidden="true"
                   >
-                    <line x1="12" y1="5" x2="12" y2="19"/>
-                    <line x1="5" y1="12" x2="19" y2="12"/>
+                    <polyline points="6 9 12 15 18 9"/>
                   </svg>
                 </button>
                 <div className="faq-answer" id={answerId}>
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import '../css/main.css'
+import './styles/main.css'
 import './index.css'
 import './styles/foundation.css'
 
